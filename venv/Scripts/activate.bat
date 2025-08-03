fb5040b2a84ef474edac0b1a87fcdf943701301d
Yusufmohamedsharif@hotmail.com <yusufmohamedsharif@hotmail.com>
2025-08-03 19:38:47 +0300

    Testing CI workflow 1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\yusuf\pcd-bahrain-1\code\Unit3\ci-and-automation-basics\venv
+set VIRTUAL_ENV=C:\Users\yusuf.DESKTOP-49J8LSA\Desktop\ci-and-automation-basics\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
